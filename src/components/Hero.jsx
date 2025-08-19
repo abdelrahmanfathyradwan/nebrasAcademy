f7c8f160059e2f8f7f876dad04aa3a117ed3627b
Abdelrahman Fathi <abdelrahmanfathi73@gmail.com>
2025-08-19 23:39:38 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
           href='https://wa.me/201146162847'
           target='_blank'
           rel='noopener noreferrer'
-          className='fixed bottom-8 right-8 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-xl transition-all animate-bounce'
+          className='fixed bottom-8 right-8 z-50 bg-blue-500 hover:bg-green-600 text-white p-4 rounded-full shadow-xl transition-all animate-bounce'
         >
           <FaWhatsapp className='text-3xl' />
         </a>
