0999c688ce007cfa4216b299bbe62ecaa0f25f76
Abdelrahman Fathi <abdelrahmanfathi73@gmail.com>
2025-08-27 12:47:45 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,32 +9,32 @@ const Hero = () => {
         {/* Background Image */}
         <div className='absolute inset-0 z-0'>
           <img
-            src='/images/heroImg.jfif' // Replace with your Quran image path
+            src='/images/hero.jpg' // Replace with your Quran image path
             alt='Quran Background'
-            className='w-full h-full object-cover object-center'
+            className='w-full h-full object-cover'
           />
-          <div className='absolute inset-0 bg-black bg-opacity-40'></div>
+          <div className='absolute inset-0 bg-white bg-opacity-20'></div>
         </div>
 
         {/* Hero Content */}
-        <div className='relative z-10 h-full flex flex-col items-center justify-center text-center px-4'>
-          <h1 className='text-4xl md:text-6xl font-bold text-orange-400 mb-4'>Nebras Academy</h1>
-          <p className='text-3xl md:text-4xl text-orange-300 mb-8 max-w-2xl'>
+        <div className='relative z-10 h-full flex flex-col items-center justify-start mt-12 text-center px-4'>
+          <h1 className='font-rb text-7xl md:text-8xl font-bold text-secondary-600 mb-4'>Nebras Academy</h1>
+          <p className='text-3xl md:text-4xl text-secondary-400 mb-8 max-w-2xl'>
             Learn Quran Online, Anytime, Anywhere
           </p>
-          <p className='text-2xl md:text-3xl text-orange-200 mb-8 max-w-2xl'>
+          <p className='text-2xl md:text-3xl text-secondary-400 mb-8 max-w-2xl'>
             Nebras Academy - Top-Rated Online Quran Classes for Kids & Adults Worldwide
           </p>
           <div className='flex gap-5'>
              <a
-          href='https://wa.me/201146162847?text=Peace%20be%20upon%20you%20and%20God%27s%20mercy%20and%20blessings.%20We%20are%20the%20Nebras%20team%20for%20Quran%20education.%20How%20can%20we%20help%20you%3F'
+          href='https://wa.me/201146162847 '
           target='_blank'
           rel='noopener noreferrer'
-          className='bg-orange-500 hover:text-orange-500 hover:bg-inherit text-white px-8 py-3 rounded-full text-lg font-semibold transition-colors shadow-lg'
+          className='bg-primary-500 hover:text-primary-600 hover:bg-inherit text-white px-5 py-2 rounded-full text-lg font-semibold transition-colors shadow-lg'
         >
-          Get a free trial now
+              Get a free trial now
         </a>
-            <Link to="/courses" className='bg-green-500 hover:bg-inherit hover:text-green-500 text-white px-8 py-3 rounded-full text-lg font-semibold transition-colors shadow-lg'>
+            <Link to="/courses" className='bg-secondary-400 hover:bg-inherit hover:text-secondary-800 text-white px-5 py-2 rounded-full text-lg font-semibold transition-colors shadow-lg'>
               Browse Our Courses
             </Link>
           </div>
@@ -45,7 +45,7 @@ const Hero = () => {
           href='https://wa.me/201146162847'
           target='_blank'
           rel='noopener noreferrer'
-          className='fixed bottom-8 right-8 z-50 bg-blue-500 hover:bg-green-600 text-white p-4 rounded-full shadow-xl transition-all animate-bounce'
+          className='fixed bottom-8 right-8 z-50 bg-secondary-400 hover:bg-secondary-600 text-white p-4 rounded-full shadow-xl transition-all animate-bounce'
         >
           <FaWhatsapp className='text-3xl' />
         </a>
